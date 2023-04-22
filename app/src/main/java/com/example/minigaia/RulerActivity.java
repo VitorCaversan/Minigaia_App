79657b9b57e1413ce474a746312a74c4e0870e5d
alfonsandrade <alfons@alunos.utfpr.edu.br>
2023-04-21 21:43:18 -0300

    Solving issue regarding seeing the data for the plantation rule. Added a example logo for the project. Added a statup animation. Added the feature that will read the JSON made by ESP32 data read by the sensors.

--- app/src/main/java/com/example/minigaia/RulerActivity.java
+++ app/src/main/java/com/example/minigaia/RulerActivity.java
@@ -1,58 +1,108 @@
 package com.example.minigaia;
 
 import android.os.Bundle;
-
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
-
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.NavigationUI;
-
-//import java.util.ArrayList;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RulerActivity extends AppCompatActivity {
-    private AppBarConfiguration appBarConfiguration;
-    private TableLayout         tableLayout;
+    private TableLayout tableLayout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ruler);
-
-        ////////////// APPBAR SETTINGS //////////////////
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        this.appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, this.appBarConfiguration);
-
-        ///////////// TABLE LAYOUT AND TEXT /////////////////////
-        this.tableLayout.findViewById(R.id.rulerTableLayout);
-        this.createTableContent();
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        // Replace the hardcoded JSON string with the actual data from your ESP32 sensor
+        String jsonString = "{\"ph\":7.2,\"desiredPh\":VaiMataTudoAsPranta,\"temperature\":25.3,\"date\":\"2023-04-21\"}";
+        SensorData sensorData = parseJsonData(jsonString);
+        this.tableLayout = findViewById(R.id.rulerTableLayout);
+        this.createTableContent(sensorData);
     }
 
-    private void createTableContent()
-    {
-        TableRow row0  = new TableRow(this);
+    private void createTableContent(SensorData sensorData) {
+        TableRow row0 = new TableRow(this);
         this.tableLayout.addView(row0);
-        TableRow row1  = new TableRow(this);
+        TableRow row1 = new TableRow(this);
         this.tableLayout.addView(row1);
-        TableRow row2  = new TableRow(this);
+        TableRow row2 = new TableRow(this);
         this.tableLayout.addView(row2);
+        TableRow row3 = new TableRow(this);
+        this.tableLayout.addView(row3);
 
-        EditText text0 = new EditText(this);
-        text0.setText("Nível Atual do Ph: ");
-        row0.addView(text0);
+        TextView actualPh = new TextView(this);
+        actualPh.setText("Nível Atual do Ph: " + sensorData.getPh());
+        actualPh.setTextSize(18);
+        row0.addView(actualPh);
 
-        EditText text1 = new EditText(this);
-        text1.setText("Nível Desejado de Ph: ");
-        row1.addView(text1);
+        TextView desiredPh = new TextView(this);
+        desiredPh.setText("Nível Desejado de Ph:" + sensorData.getDesiredPh());
+        desiredPh.setTextSize(18);
+        row1.addView(desiredPh);
 
-        EditText text2 = new EditText(this);
-        text2.setText("Nível da água: ");
+        TextView text2 = new TextView(this);
+        text2.setText("Nível da água: " + sensorData.getTemperature());
+        text2.setTextSize(18);
         row2.addView(text2);
+
+        TextView date = new TextView(this);
+        date.setText("Date " + sensorData.getDate());
+        date.setTextSize(18);
+        row3.addView(date);
+    }
+    private SensorData parseJsonData(String jsonString) {
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString);
+            double ph = jsonObject.getDouble("ph");
+            String desiredPh = jsonObject.getString("desiredPh");
+            double temperature = jsonObject.getDouble("temperature");
+            String date = jsonObject.getString("date");
+
+            return new SensorData(ph, desiredPh, temperature, date);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    private static class SensorData {
+        private final double ph;
+
+        private final String desiredPh;
+        private final double temperature;
+        private final String date;
+
+        public SensorData(double ph, String desiredPh, double temperature, String date) {
+            this.ph = ph;
+            this.desiredPh = desiredPh;
+            this.temperature = temperature;
+            this.date = date;
+        }
+
+        public double getPh() {
+            return ph;
+        }
+
+        public String getDesiredPh() {
+            return desiredPh;
+        }
+
+        public double getTemperature() {
+            return temperature;
+        }
+
+        public String getDate() {
+            return date;
+        }
     }
 }
---
+++ app/src/main/java/com/example/minigaia/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.example.minigaia;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView splashLogo = findViewById(R.id.splashLogo);
+        TextView splashText = findViewById(R.id.splashText);
+
+        AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+        fadeIn.setDuration(1000);
+        fadeIn.setFillAfter(true);
+        splashLogo.startAnimation(fadeIn);
+        splashText.startAnimation(fadeIn);
+
+        AlphaAnimation fadeOut = new AlphaAnimation(1.0f, 0.0f);
+        fadeOut.setStartOffset(2500);
+        fadeOut.setDuration(1000);
+        fadeOut.setFillAfter(true);
+        splashLogo.startAnimation(fadeOut);
+        splashText.startAnimation(fadeOut);
+
+        int SPLASH_DISPLAY_LENGTH = 4000;
+        new Handler().postDelayed(() -> {
+            Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(mainIntent);
+            finish();
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
